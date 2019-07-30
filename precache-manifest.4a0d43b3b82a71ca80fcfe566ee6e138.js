75117da5c8e4d4c8cc7b29dd40633d935bfa77a5
Binyamin Green <binyaminagreen@gmail.com>
2019-07-30 11:48:37 -0400

    Updates

---
+++ precache-manifest.4a0d43b3b82a71ca80fcfe566ee6e138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea74af3ffb3c1f5ab895de4e1f4c7847",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "d3a7fdc33b4add1694e2",
+    "url": "/todo-react/static/css/main.e04ef775.chunk.css"
+  },
+  {
+    "revision": "6df88029e8c5180a64f8",
+    "url": "/todo-react/static/js/2.13de0fc3.chunk.js"
+  },
+  {
+    "revision": "d3a7fdc33b4add1694e2",
+    "url": "/todo-react/static/js/main.13384485.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
